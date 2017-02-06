542f3db903c9d7ada77baec2de23752e4033548b
Alton Lau <altondev@gmail.com>
2017-02-06 11:23:33 -0500

    Setting up gulp build.

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,29 @@
 var gulp = require('gulp');
+var cache = require('gulp-cache');
+var cssnano = require('gulp-cssnano');
+var gulpIf = require('gulp-if');
 var del = require('del');
+var htmlmin = require('gulp-htmlmin');
+var imagemin = require('gulp-imagemin');
+var inject = require('gulp-inject');
 var jshint = require('gulp-jshint');
 var livereload = require('gulp-livereload');
+var ngAnnotate = require('gulp-ng-annotate');
 var nodemon = require('gulp-nodemon');
 var open = require('gulp-open');
 var runSequence = require('run-sequence');
 var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var useref = require('gulp-useref');
 var wait = require('gulp-wait');
 
+//==============================================================================
+// Development Tasks
+//==============================================================================
+
 // Clean task ==================================================================
-gulp.task('clean', function () {
-  del('public/css/**/*.css');
+gulp.task('clean:public', function () {
+  del.sync('public/css/**/*.css');
 });
 
 // JSHint task =================================================================
@@ -25,13 +38,24 @@ gulp.task('jshint', function () {
 });
 
 // Sass compiler task ==========================================================
-gulp.task('build-css', function () {
+gulp.task('build-sass:public', function () {
   return gulp.src([
     'public/scss/**/*.scss',
     '!public/scss/modules/**/*.scss'
   ])
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('public/css'));
+});
+
+// Inject css task =============================================================
+gulp.task('inject:public', function () {
+  return gulp.src('public/index.html')
+    .pipe(inject(gulp.src('public/css/**/*.css', {
+      read: false
+    }), {
+      relative: true
+    }))
+    .pipe(gulp.dest('public'));
 });
 
 // Node task ===================================================================
@@ -46,7 +70,16 @@ gulp.task('nodemon', function () {
 });
 
 // Reload task =================================================================
-gulp.task('reload', ['jshint', 'build-css'], function () {
+gulp.task('reload-css', function (done) {
+  runSequence('build-sass:public', 'inject-css:public', 'refresh', done);
+});
+
+gulp.task('reload-js', function (done) {
+  runSequence('jshint', 'refresh', done);
+});
+
+// Refresh page task ===========================================================
+gulp.task('refresh', function () {
   gulp.src('./')
     .pipe(wait(1000))
     .pipe(livereload());
@@ -55,10 +88,12 @@ gulp.task('reload', ['jshint', 'build-css'], function () {
 // Watch task ==================================================================
 gulp.task('watch', function () {
   livereload.listen();
+  gulp.watch('public/**/*.html', ['refresh']);
+  gulp.watch('public/scss/**/*.scss', ['reload-css']);
   gulp.watch([
-    'server/**/*.js',
-    'public/**/*.{html,scss,js}'
-  ], ['reload']);
+    'public/**/*.js',
+    'server/**/*.js'
+  ], ['reload-js']);
 });
 
 // Open task ===================================================================
@@ -71,7 +106,49 @@ gulp.task('open', function () {
     }));
 });
 
-// Main task ===================================================================
-gulp.task('default', function (done) {
-  runSequence('clean', 'jshint', 'build-css', ['nodemon', 'watch'], 'open', done);
+//==============================================================================
+// Build Tasks
+//==============================================================================
+
+// Clean task ==================================================================
+gulp.task('clean:dist', function () {
+  del.sync('dist');
 });
+
+// HTML minify task ============================================================
+gulp.task('build-views:dist', function () {
+  return gulp.src('public/views/**/*.html')
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
+    .pipe(gulp.dest('dist/views'));
+});
+
+// Image minify task ===========================================================
+gulp.task('build-images:dist', function () {
+  return gulp.src('public/assets/**/*.+(png|jpg|jpeg|gif|svg)')
+    .pipe(cache(imagemin()))
+    .pipe(gulp.dest('dist/images'));
+});
+
+// Javascript minify task ======================================================
+gulp.task('build:dist', function () {
+  return gulp.src('public/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', ngAnnotate()))
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'));
+});
+
+//==============================================================================
+// Runnable Tasks
+//==============================================================================
+
+gulp.task('default', function (done) {
+  runSequence('clean:public', ['jshint', 'build-sass:public'], 'inject:public', ['nodemon', 'watch'], 'open', done);
+});
+
+gulp.task('build', function (done) {
+  runSequence('clean:dist', 'build-sass:public', 'inject:public', ['build:dist', 'build-views:dist', 'build-images:dist'], done);
+})
