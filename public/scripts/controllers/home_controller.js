65d937a66bdb34e290a00057e0045a8d0bc94c97
Alton Lau <altondev@gmail.com>
2017-03-05 21:59:42 -0500

    Prepared home page buttons.

--- public/scripts/controllers/home_controller.js
+++ public/scripts/controllers/home_controller.js
@@ -7,7 +7,82 @@
 
 angular.module('altonApp').controller('HomeController', function ($scope, websiteService) {
 
+  var disableClick = false;
+
   $scope.maintenance = null;
+
+  $scope.buttonHovered = function (event, over) {
+    if (!$scope.mobile()) {
+      var title = $($(event.currentTarget).siblings('.button-title')[0]);
+      title.css('opacity', over ? 1 : 0);
+    }
+  };
+
+  $scope.moveToProjectPage = function (event) {
+    if (!disableClick) {
+      disableClick = !disableClick;
+
+      var button = $(event.currentTarget);
+      var parent = button.parent();
+
+      var fill = $('<span></span>');
+      fill.css({
+        'background': button.css('background'),
+        'border-radius': button.height() / 2,
+        'height': button.height(),
+        'position': 'absolute',
+        'left': button.position().left + button.width() / 2,
+        'top': button.position().top + button.height() / 2,
+        'transform': 'translate(-50%, -50%)',
+        'width': button.width(),
+        'z-index': -1
+      });
+
+      fill.insertBefore(button);
+      parent.css('z-index', 1);
+
+      fill.animate({
+        'border-radius': '200vh',
+        height: '400vh',
+        width: '400vh'
+      }, 'slow', function () {
+        // TODO: Move to project page
+      });
+    }
+  };
+
+  $scope.moveToSkillPage = function (event) {
+    if (!disableClick) {
+      disableClick = !disableClick;
+
+      var button = $(event.currentTarget);
+      var parent = button.parent();
+
+      var fill = $('<span></span>');
+      fill.css({
+        'background': button.css('background'),
+        'border-radius': button.height() / 2,
+        'height': button.height(),
+        'position': 'absolute',
+        'left': button.position().left + button.width() / 2,
+        'top': button.position().top + button.height() / 2,
+        'transform': 'translate(-50%, -50%)',
+        'width': button.width(),
+        'z-index': -1
+      });
+
+      fill.insertBefore(button);
+      parent.css('z-index', 1);
+
+      fill.animate({
+        'border-radius': '200vh',
+        height: '400vh',
+        width: '400vh'
+      }, 'slow', function () {
+        // TODO: Move to skill page
+      });
+    }
+  };
 
   function loadWebsiteStats() {
     websiteService.maintenance().then(function (response) {
