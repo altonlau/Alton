1b2a46f830b0dfa14895fdc290be61b002abd1bd
Alton Lau <altondev@gmail.com>
2017-03-18 12:16:05 -0400

    Implementing projects public page.

--- public/scripts/controllers/project_controller.js
+++ public/scripts/controllers/project_controller.js
@@ -5,9 +5,10 @@
  * Description: Project controller
  */
 
-angular.module('altonApp').controller('ProjectController', function ($scope, $state, $timeout, projectFactory, websiteService) {
+angular.module('altonApp').controller('ProjectController', function ($scope, $state, $timeout, projectFactory, skillFactory, websiteService) {
 
   $scope.projects = null;
+  $scope.skills = null;
 
   $scope.moveToHomePage = function () {
     $scope.projects = null;
@@ -17,13 +18,24 @@ angular.module('altonApp').controller('ProjectController', function ($scope, $st
     }, 200);
   };
 
+  $scope.viewProject = function (project) {
+    $('html, body').animate({
+      scrollTop: $('#' + project.id).offset().top
+    });
+  };
+
   function setup() {
     websiteService.maintenance().then(function (response) {
       if (response) {
         $state.go('home');
       } else {
         websiteService.load().then(function () {
-          $scope.projects = projectFactory.getAll();
+          $scope.projects = projectFactory.getAll().map(function (project) {
+            project.description = marked(project.description);
+            return project;
+          });
+
+          $scope.skills = skillFactory.getAll();
         }, function () {
           // TODO: Whoops page.
         });
