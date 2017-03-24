88e0d2a3d0c3a6a64c80914ec8cd05e5cc482ed5
Alton Lau <altondev@gmail.com>
2017-03-23 23:25:08 -0400

    Added more buttons on home page and a speech bubble.

--- public/scripts/controllers/home_controller.js
+++ public/scripts/controllers/home_controller.js
@@ -5,11 +5,28 @@
  * Description: Home controller
  */
 
-angular.module('altonApp').controller('HomeController', function ($scope, stateTransitionService, websiteService) {
+angular.module('altonApp').controller('HomeController', function ($cookies, $scope, $state, $timeout, stateTransitionService, websiteService) {
 
+  var cookieMute = 'mute_alton';
   var disableClick = false;
+  var speechList = [{
+    text: 'Hello! I\'m Alton Lau!',
+    next: 1
+  }, {
+    text: 'Click any button including me!',
+    next: null,
+  }, {
+    text: 'How is your day today?',
+    next: null
+  }, {
+    text: 'I\'m a mobile developer.',
+    next: null
+  }];
+  var prevSpeech = null;
 
   $scope.maintenance = null;
+  $scope.speech = null;
+  $scope.mute = JSON.parse($cookies.get(cookieMute) || false);
 
   $scope.buttonHovered = function (event, over) {
     if (!$scope.mobile()) {
@@ -43,6 +60,40 @@ angular.module('altonApp').controller('HomeController', function ($scope, stateT
     stateTransitionService.transition('404');
   };
 
+  $scope.toggleMute = function () {
+    $scope.mute = !$scope.mute;
+    $cookies.put(cookieMute, JSON.stringify($scope.mute));
+
+    if ($scope.mute) {
+      $scope.speech = null;
+    } else {
+      cycleSpeech();
+    }
+  };
+
+  function cycleSpeech() {
+    var delay;
+
+    if (!$scope.mute && $state.is('home')) {
+      if ($scope.speech) {
+        delay = $scope.speech.next ? 500 : randomNumber(5000, 10000);
+        prevSpeech = $scope.speech;
+        $scope.speech = null;
+      } else {
+        delay = 5000;
+        $scope.speech = speechList[prevSpeech ? prevSpeech.next || randomNumber(1, speechList.length) : 0];
+      }
+
+      $timeout(function () {
+        cycleSpeech();
+      }, delay);
+    }
+  }
+
+  function randomNumber(min, max) {
+    return Math.floor(Math.random() * max) + min;
+  }
+
   function loadWebsite() {
     websiteService.maintenance().then(function (response) {
       $scope.maintenance = response;
@@ -53,7 +104,14 @@ angular.module('altonApp').controller('HomeController', function ($scope, stateT
             websiteService.viewed();
           }
         }, {});
-        websiteService.load().then({}, function () {
+        websiteService.load().then(function () {
+          $timeout(function () {
+            cycleSpeech();
+          }, 2000);
+          $timeout(function () {
+            $scope.canStopTalking = true;
+          }, 60000);
+        }, function () {
           $scope.moveTo404Page();
         });
       }
